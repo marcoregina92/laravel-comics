0c5e7e070c31524ab4448df6161b6e1d0f1f0f07
Marco <marcoregina92@gmail.com>
2024-01-19 18:58:26 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel-comics</title>
+</head>
+<body>
+
+
+    <header>
+        <a href="/">Homepage</a>
+    </header>
+
+    <h1> {{ $msg }}  {{ $title }} </h1>
+    
+</body>
+</html>
